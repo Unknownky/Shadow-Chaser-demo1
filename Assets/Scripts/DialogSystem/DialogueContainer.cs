afb6924cdec2e16d9725b639a8934e15e514178d
BuZhiDao <3085017810@qq.com>
2024-10-10 21:11:04 +0800

    添加一键添加Dialogue功能，添加场景一对话

--- Assets/Scripts/DialogSystem/DialogueContainer.cs
+++ Assets/Scripts/DialogSystem/DialogueContainer.cs
@@ -7,4 +7,25 @@ public class DialogueContainer : ScriptableObject
 {
     [SerializeField] public List<Dialogue> dialogues = new List<Dialogue>();
 
+
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("Developer/Add All Dialogues")]
+    //添加当前文件夹下所有的对话
+    public static void AddAllDialogues()
+    {
+        DialogueContainer container = Resources.Load<DialogueContainer>("DialogueContainer");
+        container.dialogues.Clear();
+
+        string[] guids = UnityEditor.AssetDatabase.FindAssets("t:Dialogue", new[] { "Assets/Scripts/DialogSystem/DialogFiles" });
+        foreach (string guid in guids)
+        {
+            string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+            Dialogue dialogue = UnityEditor.AssetDatabase.LoadAssetAtPath<Dialogue>(path);
+            container.dialogues.Add(dialogue);
+        }
+
+        UnityEditor.EditorUtility.SetDirty(container);
+    }
+#endif
+
 }
