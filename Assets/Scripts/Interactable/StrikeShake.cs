e772f3ed82660335c9c56ce4b6b4e5fa7172ebe3
BuZhiDao <3085017810@qq.com>
2024-10-16 22:35:10 +0800

    修正CinemaShake获取错误的问题

--- Assets/Scripts/Interactable/StrikeShake.cs
+++ Assets/Scripts/Interactable/StrikeShake.cs
@@ -9,6 +9,14 @@ public class StrikeShake : MonoBehaviour
     [SerializeField] private float time = 0.3f;
 
     private void OnCollisionEnter2D(Collision2D other) {
+        // GameObject boxCanvas = GameObject.Find("BagCanvas");
+        //获取Screen Space - Camera模式下的Canvas组件的RenderCamera
+        // Camera renderCamera = boxCanvas.GetComponent<Canvas>().worldCamera;
+        Camera mainCamera = Camera.main;
+        //获取当前Camera的CinemachineBrain组件的ActiveVirtualCamera
+        Cinemachine.CinemachineBrain brain = mainCamera.GetComponent<Cinemachine.CinemachineBrain>();
+        vcam = brain.ActiveVirtualCamera.VirtualCameraGameObject.GetComponent<CinemaShake>();
+
         vcam.ShakeCamera(intensity, time);
     }
 }   
